4bd387cc20b3a1f74f277f78248f296aa7da5ce4
Mike Botsko <botsko@gmail.com>
2014-07-03 12:23:59 -0700

    Improvements/fixes for ip bans. Allowing user to name online player to auto-read the IP

--- src/main/java/me/botsko/oracle/commands/BanIpCommand.java
+++ src/main/java/me/botsko/oracle/commands/BanIpCommand.java
@@ -1,6 +1,8 @@
 package me.botsko.oracle.commands;
 
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import me.botsko.oracle.Oracle;
 import me.botsko.oracle.commandlibs.CallInfo;
@@ -16,11 +18,28 @@ public class BanIpCommand implements SubHandler {
 	public void handle(CallInfo call) {
 		
 		if(call.getArgs().length <= 0){
-			call.getSender().sendMessage( Oracle.messenger.playerError("You must provide an IP address to ban.") );
+			call.getSender().sendMessage( Oracle.messenger.playerError("You must provide a player name or IP address to ban.") );
 			return;
 		}
 		
-		if(call.getArg(0).equals("127.0.0.1")){
+		String ip = call.getArg(0);
+
+		String ip_regex = "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+		if( !ip.matches( ip_regex ) ){
+		    
+		    // Try to find player
+		    Player player = Bukkit.getPlayer( ip );
+		    
+		    if( player == null ){
+		        call.getSender().sendMessage( Oracle.messenger.playerError("No player online named " + ip) );
+	            return;
+		    }
+		    
+		    ip = player.getAddress().getAddress().toString().replace( "/", "" );
+		    
+		}
+
+		if( ip.equals("127.0.0.1") ){
 			call.getSender().sendMessage( Oracle.messenger.playerError("You may not ban a localhost IP.") );
 			return;
 		}
@@ -35,10 +54,22 @@ public class BanIpCommand implements SubHandler {
 		}
 
 		// Save to db
-		BanUtil.banByIp( call.getSender(), call.getArg(0), reason );
+		BanUtil.banByIp( call.getSender(), ip, reason );
 		
-		// Tell banner
-		call.getSender().sendMessage( Oracle.messenger.playerHeaderMsg( call.getSender().getName() + " banned IP " + call.getArg(0) + " for: " + reason ) );
+		// Kick all players with this IP
+		for( Player p : Bukkit.getOnlinePlayers() ){
+		    String p_ip = p.getAddress().getAddress().toString().replace( "/", "" );
+		    if( p_ip.equals( ip ) ){
+		        p.kickPlayer( reason );
+		    }
+		}
+		
+		// Tell peeps
+		for( Player p : Bukkit.getOnlinePlayers() ){
+            if( p.hasPermission( "oracle.bans.alert" ) ){
+                p.sendMessage( Oracle.messenger.playerHeaderMsg( call.getSender().getName() + " banned IP " + ip + " for: " + reason ) );
+            }
+        }
     
 	}
 }
