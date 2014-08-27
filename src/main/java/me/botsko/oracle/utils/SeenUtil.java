6030f8ba70763632dd49926a63d4d669bc211f08
Mike Botsko <botsko@gmail.com>
2014-08-27 16:56:35 -0700

    Adding getPlayerLastJoin to api

--- src/main/java/me/botsko/oracle/utils/SeenUtil.java
+++ src/main/java/me/botsko/oracle/utils/SeenUtil.java
@@ -54,6 +54,45 @@ public class SeenUtil {
 	}
 	
 	/**
+     * 
+     * @param person
+     * @param account_name
+     * @throws Exception 
+     */
+    public static Date getPlayerLastJoin( OfflinePlayer player ) throws Exception{
+        Date seen = null;
+        Connection conn = null;
+        PreparedStatement s = null;
+        ResultSet rs = null;
+        try {
+            
+            // Insert/Get Player ID
+            PluginPlayer pluginPlayer = PlayerIdentification.getOraclePlayer( player.getName() );
+            if( pluginPlayer == null ){
+                throw new Exception("Player has never played on this server.");
+            }
+                        
+            conn = Oracle.dbc();
+            s = conn.prepareStatement ("SELECT player_join FROM oracle_joins j WHERE player_id = ? ORDER BY player_join DESC LIMIT 1;");
+            s.setInt(1, pluginPlayer.getId());
+            s.executeQuery();
+            rs = s.getResultSet();
+            
+            if(rs.first()){
+                seen = new Date(rs.getLong("player_quit") * 1000);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(rs != null) try { rs.close(); } catch (SQLException e) {}
+            if(s != null) try { s.close(); } catch (SQLException e) {}
+            if(conn != null) try { conn.close(); } catch (SQLException e) {}
+        }
+        return seen;
+    }
+	
+	/**
 	 * 
 	 * @param person
 	 * @param account_name
